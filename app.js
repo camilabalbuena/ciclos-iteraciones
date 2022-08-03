7f58e64e96af2ca30db6c45b25eff3998a82ca4b
Camila Balbuena <camilabalbuena99@gmail.com>
2022-08-02 23:50:39 -0300

    subiendo archivo

---
+++ app.js
@@ -0,0 +1,6 @@
+let alumnos = prompt("Ingresa el nombre del alumno para conocer su asistencia o ESC para terminar")
+while(alumnos != "esc"){
+    
+    console.log (alumnos + "se encuentra presente")
+    alumnos = prompt("Ingresa el nombre del alumno para conocer su asistencia o ESC para terminar")
+}
