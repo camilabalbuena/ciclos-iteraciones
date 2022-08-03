8789020862d5988ab4fc9a4e861f99bd254ad8c6
Camila Balbuena <camilabalbuena99@gmail.com>
2022-08-03 00:01:26 -0300

    se modifico js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let alumnos = prompt("Ingresa el nombre del alumno para conocer su asistencia o ESC para terminar")
-while(alumnos != "esc"){
-    
-    console.log (alumnos + "se encuentra presente")
+while(alumnos != "esc", alumnos != "ESC"){
+    alert (alumnos + " se encuentra presente.")
     alumnos = prompt("Ingresa el nombre del alumno para conocer su asistencia o ESC para terminar")
 }
+
